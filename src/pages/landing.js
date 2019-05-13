f0f48b39387cbf3f5ca5a5414d4a897fd649030e
Karlo <karloe@gmail.com>
2019-05-13 16:28:47 -0400

    more stlyle fixes in landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -21,7 +21,7 @@ export default function landing() {
       <div className="jumbotron text-center">
         <h1 className="font-weight-bold">Meal Planning?</h1>
         <p>
-          We'll narrow down the right options for you and your family based from over 300,000 recipes.
+          We narrow down the right options for you and your family based from over 300,000 recipes.
         </p>
         <p className="lead">
           <button className="btn btn-primary btn-lg" href="#" role="button">
@@ -35,56 +35,43 @@ export default function landing() {
 
         {/* RECOMMENDED FOR YOU MD UP*/}
         <div className="d-none d-md-block">
-        <h3>Recommended for you</h3>
+        <h3>Recommended diets for you</h3>
         <div className="row">
           <div className="col-md-3 d-none d-md-block">
           <div className="card">
-            <img
-              className="card-img-top img-fluid" src={meal_1} alt="Card image cap"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+            <img className="card-img-top img-fluid" src={meal_3} alt="Ketogenic" />
+            <div className="recommended-caption text-center">
+              <h4 className="recommended-title">Ketogenic</h4>
+              <span className="recommended-text">High fat &#38; protien, low carbs</span>
+              </div>
             </div>
+            
+          </div>
+          <div className="col-md-3 d-none d-md-block">
+            <div className="card">
+              <img className="card-img-top img-fluid" src={meal_2} alt="Pescatarian" />
+              <div className="recommended-caption text-center">
+              <h4 className="recommended-title">Pescatarian</h4>
+              <span className="recommended-text">Fish &#38; Veggies</span>
+              </div>
             </div>
           </div>
           <div className="col-md-3 d-none d-md-block">
           <div className="card">
-            <img className="card-img-top img-fluid" src={meal_2} alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
+              <img className="card-img-top img-fluid" src={meal_1} alt="Card image cap"/>
+              <div className="recommended-caption text-center">
+              <h4 className="recommended-title">Vegan</h4>
+              <span className="recommended-text">Fish &#38; Veggies</span>
+              </div>
             </div>
           </div>
           <div className="col-md-3 d-none d-md-block">
           <div className="card">
-            <img className="card-img-top img-fluid" src={meal_3} alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-            </div>
-          </div>
-          <div className="col-md-3 d-none d-md-block">
-          <div className="card">
-            <img className="card-img-top img-fluid" src={meal_4} alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
+            <img className="card-img-top img-fluid" src={meal_4} alt="Whole 30" />
+            <div className="recommended-caption text-center">
+              <h4 className="recommended-title">Whole 30&#174;</h4>
+              <span className="recommended-text">Follows the program rule</span>
+              </div>
             </div>
           </div>
         </div>
@@ -116,6 +103,7 @@ export default function landing() {
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
+                the bulk of the card's content. Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
             </div>
@@ -150,12 +138,57 @@ export default function landing() {
         </div>
 
         <br/>
-        <h3>Your heath matters</h3>
+        <br/>
+        <br/>
+
+
+
+        <div className="row row-eq-height">
+          <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h6 className="card-title">It's Free</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h6 className="card-title">Personalized Menus</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content. Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h6 className="card-title">Easy to Use</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+            </div>
+          </div>
+        </div>
+
+
+
 
         <br/>
         <br/>
 
         <h3 className="text-center">How it works</h3>
+        <br/>
+
         <div className="row row-eq-height">
           <div className="col-xs-12 col-sm-6">
             <strong>Lorem Ipsum</strong>
@@ -172,19 +205,32 @@ export default function landing() {
             </p>
           </div>
 
-          <div className="col-xs-12 col-sm-6" />
-        </div>
-        <div className="row row-eq-height">
-          <div className="col-xs-12 col-sm-6" />
+          <div className="card col-xs-12 col-sm-6">
 
-          <div className="col-xs-12 col-sm-6" />
-          <strong>Incididunt ut labore</strong>
-          <p>
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua.
-          </p>
+          </div>
+
+
+        </div>
+        <br/>
+        <div className="row row-eq-height">
+          <div className="card col-xs-12 col-sm-6">
+
+          </div>
+
+          <div className="col-xs-12 col-sm-6">
+            <strong>Lorem Ipsum</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </div>
 
         <br/>
@@ -197,9 +243,9 @@ export default function landing() {
         <br/>
         
       </div>
-      <div className="container-fluid landing-footer">
-          
-        </div>
+      <div className="container-fluid landing-footer text-center">
+        <span className="align-middle">&#xa9; Cook Up, 2019</span>
+      </div>
     </div>
   );
 }
