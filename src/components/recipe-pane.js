7dddce10e56e686f35f4cd7ff78b26cc2a397232
Karlo Eleuterio <karloe@gmail.com>
2019-06-07 00:11:52 -0400

    show ingredients and directions on reciep pane

--- src/components/recipe-pane.js
+++ src/components/recipe-pane.js
@@ -20,16 +20,41 @@ function RecipePane(props) {
                     <div className="col col-12 col-md-6">
                         <h3>{recipe.title}</h3>
                         <p>
-                                                {recipe.readyInMinutes &&
-                                                    <span title="preparation and cooking time">{recipe.readyInMinutes} mins &nbsp; | &nbsp; </span>
-                                                }
-                                                {recipe.servings &&
-                                                    <span title="no. of servings"> <Icon size={.7} path={mdiAccountGroup} /> <span> {recipe.servings}  &nbsp; | &nbsp;  </span></span>
-                                                }
-                                                {recipe.glutenFree &&
-                                                    <span className="gluten-free" title="*gluten-free"> <Icon size={.7} path={mdiBarleyOff} /> </span>
-                                                }
-                                            </p>
+                            {recipe.readyInMinutes &&
+                                <span title="preparation and cooking time">{recipe.readyInMinutes} mins &nbsp; | &nbsp; </span>
+                            }
+                            {recipe.servings &&
+                                <span title="no. of servings"> <Icon size={.7} path={mdiAccountGroup} /> <span> {recipe.servings}  &nbsp; | &nbsp;  </span></span>
+                            }
+                            {recipe.glutenFree &&
+                                <span className="gluten-free" title="*gluten-free"> <Icon size={.7} path={mdiBarleyOff} /> </span>
+                            }
+                        </p>
+                    </div>
+                </div>
+                <div className="row row-ingredients">
+                    <div className="col col-12">
+                        <h5>Ingredients</h5>
+                        <ul className="list-group list-group-flush">
+                            {recipe.extendedIngredients.map((ing, i) => (
+                                <li key={i} className="list-group-item d-flex justify-content-between align-items-center">
+                                    {ing.name}
+                                    <span className="">{ing.amount} {ing.unit}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="row row-steps">
+                    <div className="col col-12">
+                        <h5>Directions</h5>
+                        <ul className="list-group list-group-flush">
+                            {recipe.analyzedInstructions[0].steps.map((ing, i) => (
+                                <li key={i} className="list-group-item d-flex">
+                                    <strong className="step-num">{i + 1}.</strong>{ing.step}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 
 
-export const fcUrl = true ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
+export const fcUrl = process.env.NODE_ENV === "production" ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
 
-export const host = true ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
+export const host = process.env.NODE_ENV === "production" ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
+
+
+// export const fcUrl = true ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
+
+// export const host = true ? "https://us-central1-fo-db-e5cab.cloudfunctions.net/" : "http://localhost:5000/fo-db-e5cab/us-central1/";
