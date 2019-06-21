3339c0887392e18afa040d23e5696b9a6537f3eb
Karlo Eleuterio <karloe@gmail.com>
2019-06-21 01:15:15 -0400

    added status page foe redirecting user to right page

--- src/pages/status.js
+++ src/pages/status.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux';
 import { Redirect, Link } from "react-router-dom";
 import moment from 'moment';
 import foHttp from '../helpers/fohttp';
-import dectoFrac from '../helpers/decimal-fraction';
+
 import propTypes from "prop-types";
-import ProgressBar from "../components/progress-view";
-import CalendarIndicator from "../components/calendar-indicator";
-import AccountPane from "../components/account-pane";
-import ReferPane from "../components/refer-pane";
+
+
 import { updateHasPreferences, updateHasOptions } from '../actions/progressActions';
 
 import Icon from "@mdi/react";
@@ -25,86 +23,60 @@ class status extends Component {
         this.state = {
             week: moment().week(),
             year: moment().year(),
-            hasSuggestions: false,
-            hasPreferences: false
         };
-
-
     }
 
     componentDidMount() {
 
+
         const { week, year } = this.state;
+
 
         let params = {
             week,
             year
         };
-
         foHttp("GET", "status", params).then(res => {
-            console.log(res.data);
             if (res.success) {
-                if (res.data.hasSuggestions) {
-                    this.setState({ hasSuggestions: true });
-                } else if (res.data.hasPreferences) {
-                    this.setState({ hasPreferences: true });
-                } else {
+                if (res.data.hasChosen) {
+                    this.setState({ hasChosen: true });
                     this.props.updateHasOptions(true);
                     this.props.updateHasPreferences(true);
-                    this.setState({ ingredients: res.data.ingredients })
+                } else if (res.data.hasPreferences) {
+                    this.setState({ hasPreferences: true });
+                    this.props.updateHasPreferences(true);
+                } else {
+                    this.setState({ hasPreferences: false });
                 }
             }
         })
-
-
-
     }
-
-    changeWeek(date) {
-        console.log(`changing week to: ${date}`); // TODO: use this event later when user wants to see previous meals
-    }
-
-    handleRadioChange = (ing, i) => event => {
-        this.setState(prevState => {
-            const ingredients = [...prevState.ingredients];
-            ingredients[i].cart = !prevState.ingredients[i].cart;
-
-            let params = {
-                ingredients,
-                week: prevState.week,
-                year: prevState.year
-            }
-            foHttp("PUT", "groceries", params);
-            return {
-                ingredients
-            };
-        });
-
-
-
-    };
-
-
 
     render() {
 
-        const { progress, currentUser } = this.props;
-        const { noSelection, noSuggestions, ingredients } = this.state;
+        const { isAuth } = this.props.currentUser;
+        const { hasChosen, hasPreferences, } = this.state;
 
-        if (noSelection || noSuggestions) {
+        if (!isAuth) {
+            return <Redirect to="/login" />
+        }
+
+        if (hasChosen) {
+            return <Redirect to="/my-meals" />
+        }
+
+        if (hasPreferences) {
             return <Redirect to="/my-options" />
         }
 
-
-
-
+        if (hasPreferences === false) {
+            return <Redirect to="/my-preferences" />
+        }
 
         return (
-
             <div className="container page-main groceries-page">
 
             </div>
-
         )
     }
 }
